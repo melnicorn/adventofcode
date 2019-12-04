5014db5845b402ee0cf1851e9d0a0326f348efe4
Chris Melnick <cmelnick@gmail.com>
2019-12-04 09:37:35 -0600

    hi

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"day02"
-)
-
-func main() {
-	day02.FuelUsage()r
-}
---
+++ src/app/entry.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"adventofcode"
+	"fmt"
+)
+
+func main() {
+	// fmt.Println("****** DAY 01 **********************")
+	// fmt.Printf("\tPart 1: %f\n", adventofcode.Day01Part1())
+	// fmt.Printf("\tPart 2: %f\n", adventofcode.Day01Part2())
+
+	// fmt.Println("****** DAY 02 **********************")
+	// fmt.Printf("\tPart 1: %d\n", adventofcode.Day02Part1())
+	// noun, verb := adventofcode.Day02Part2()
+	// fmt.Printf("\tPart 2: 100 * %d + %d = %d\n", noun, verb, ((100 * noun) + verb))
+
+	fmt.Println("****** DAY 03 **********************")
+	fmt.Printf("\tPart 1: %d\n", adventofcode.Day03Part1())
+}
